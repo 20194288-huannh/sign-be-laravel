326c24678ef5f722465cc9fbc3afc3507dda6d1d
Nguyen Huu Huan <huan.nguyen2@kiaisoft.com.vn>
2024-03-22 10:10:53 +0700

    feature: response marco

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -17,8 +18,36 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
+    const SUCCESS = 1;
+    const FAIL = 1;
     public function boot(): void
     {
-        //
+        Response::macro('ok', function ($code, $data = null) {
+            if (!empty($data)) {
+                if (is_array($data)) {
+                    $data = count($data) > 0 ? $data : null;
+                } elseif (is_object($data)) {
+                    $data = $data->count() > 0 ? $data : null;
+                } else {
+                    $data = $data ? $data : null;
+                }
+            }
+
+            $output = [
+                'success' => self::SUCCESS,
+                'data' => $data,
+                'errors' => null
+            ];
+            return response()->json($output, $code);
+        });
+
+        Response::macro('error', function ($code, $message, $data = null) {
+            $output = [
+                'success' => self::FAIL,
+                'data' => $data,
+                'errors' => $message
+            ];
+            return response()->json($output, $code);
+        });
     }
 }
